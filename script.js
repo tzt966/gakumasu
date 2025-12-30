22931d30ea801d5cca1bb551089dcb80f87461f2
tzt966 <tzt966@gmail.com>
2025-12-31 05:56:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ const start = document.getElementById("start");
 start.addEventListener("click", async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "environment" }
+      video: {
+         facingMode: "environment" ,
+         width: { ideal: 1179 },
+         height: { ideal: 2556 },
+         frameRate: { ideal: 30 }
+    },
+    audio: false
     });
     video.srcObject = stream;
   } catch (err) {
     alert("カメラを起動できません");
   }
+});
+
+const select = document.getElementById('character');
+select.addEventListener('change', ()=> {
+    select.className = select.value
 });
